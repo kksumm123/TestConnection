fd98c17f63e45f612301f6f5e46ca812c2b3c002
Woony <kksumm123@gmail.com>
2023-06-20 21:33:47 +0900

    Initial commit

---
+++ Assets/_Script/MainSystem/BackendManager.cs
@@ -0,0 +1,21 @@
+﻿using BackEnd;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackendManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        var backend = Backend.Initialize(true);
+
+        if (backend.IsSuccess())
+        {
+            Debug.Log($"성공: {backend}");
+        }
+        else
+        {
+            Debug.Log($"실패: {backend}");
+        }
+    }
+}
